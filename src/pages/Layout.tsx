3fd34fc8395d67985bf72f1cbce8dfd8090255da
lugen4ro <54658759+lugen4ro@users.noreply.github.com>
2024-03-14 12:02:52 +0900

    Add attribution to RAWG.io

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Link } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
@@ -6,6 +6,15 @@ const Layout = () => {
     return (
         <>
             <NavBar />
+            <Flex justify="end" mr="1rem" my="-1rem">
+                <Link
+                    color="gray"
+                    href="https://rawg.io/apidocs"
+                    target="_blank"
+                >
+                    All data/images from RAWG.io
+                </Link>
+            </Flex>
             <Box padding={5}>
                 <Outlet />
             </Box>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axisoInstance = axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "2ee08beb4dec4a89a38d9de142561121",
+        key: "2ee08beb4dec4a89a38d9de142561121", // Not good putting the api key here, but since we have not backend, we must compromise
     },
 });
 
